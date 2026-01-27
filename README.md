85cae299ca6147f3bf5db74b32901ea2743f3879
Will <7099514+Willburd@users.noreply.github.com>
2026-01-26 22:35:41 -0500

    wipe

--- README.md
+++
@@ -1,2 +0,0 @@
-# The Old Adventure
-A game engine aimed at recreating the style of older 3D action adventure games.
--- src/Engine/Components/ActorBehaviors/BehaviorStateMachines/CameraStates/CameraState.cs
+++
@@ -1,9 +0,0 @@
-using Engine;
-
-namespace EntComponents.ActorBehavior.CameraStates
-{
-    public class CameraState(PlayerCameraBehavior owner) : BehaviorStateMachine(owner)
-    {
-        protected float distance_from_player = 6.5f;
-    }
-}
