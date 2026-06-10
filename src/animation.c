179230f309fbdfba42fb8ec3579b06977e8498aa
Will <7099514+Willburd@users.noreply.github.com>
2026-06-10 08:16:31 -0400

    still not working animation, but recursive bone solver

--- src/animation.c
+++ src/animation.c
@@ -110,62 +110,105 @@ void UpdateAnimLayers(struct Actor* actor)
     }
 }
 
+
+
 #define ANIM_MIN_THESHOLD 0.00001f
 #define CHECK_SKIP_LAYER(x) !x->is_playing || x->blend_factor <= ANIM_MIN_THESHOLD
+
+void ApplyAnimationLayerTransformsToBone(int bone_index, Model* model, struct Actor* actor)
+{
+    BoneInfo* bone = &model->skeleton.bones[bone_index];
+
+    // Reset bones to zero by applying the inverse of their bind position
+    Transform* bind_transform = &model->skeleton.bindPose[bone_index];
+    Matrix bind_matrix = MatrixMultiply(MatrixMultiply(
+        MatrixScale(bind_transform->scale.x, bind_transform->scale.y, bind_transform->scale.z),
+        QuaternionToMatrix(bind_transform->rotation)),
+        MatrixTranslate(bind_transform->translation.x, bind_transform->translation.y, bind_transform->translation.z));
+    model->boneMatrices[bone_index] = MatrixInvert(bind_matrix); // Apply bind pose to bone before we do any transforms
+
+    // Summed transformation of all blended animations
+    Transform blended_transform = {
+        .translation = Vector3Zero(),
+        .rotation = QuaternionIdentity(),
+        .scale = Vector3One()
+    };
+
+    // Blend all active layers by their actual blending percents
+    for (int i = 0; i <= actor->animlayer_count; i++)
+    {
+        if (CHECK_SKIP_LAYER(actor->animation_layers[i])) // Layer is innactive.
+            continue;
+        struct AnimationLayer* layer = actor->animation_layers[i];
+        if (!layer->bone_filter[bone_index]) // Layer is disabled by this bone's filter.
+            continue;
+
+        // Get current bone's transform at this frame of the animation
+        ModelAnimation* anim = layer->current_animation;
+        int frame_index = (int)layer->current_frame;
+        Transform* anim_transform = &anim->keyframePoses[frame_index][bone_index];
+
+        // Lerp from the prior pose to the desired transform. This will act as our influence of intensity.
+        blended_transform.translation = Vector3Lerp(blended_transform.translation, anim_transform->translation, layer->blend_factor);
+        blended_transform.rotation = QuaternionSlerp(blended_transform.rotation, anim_transform->rotation, layer->blend_factor);
+        blended_transform.scale = Vector3Lerp(blended_transform.scale, anim_transform->scale, layer->blend_factor);
+    }
+
+    /*
+    // Parent bones keep the binding pose's affect
+    if (bone->parent != -1)
+    {
+        // Remove binding pose from the final transform
+        blended_transform.translation = Vector3Subtract(blended_transform.translation, bind_transform->translation);
+        blended_transform.rotation = QuaternionMultiply(blended_transform.rotation, QuaternionInvert(bind_transform->rotation));
+        blended_transform.scale = Vector3Multiply(blended_transform.scale, Vector3Scale(bind_transform->scale, -1));
+
+        // Get parent's transform and merge it in
+        Transform parent_transform;
+        MatrixDecompose(model->boneMatrices[bone->parent], &parent_transform.translation, &parent_transform.rotation, &parent_transform.scale);
+
+        // Apply our transform to parent's to get our real position
+        blended_transform.translation = Vector3Add(blended_transform.translation, parent_transform.translation);
+        blended_transform.rotation = QuaternionMultiply(blended_transform.rotation, parent_transform.rotation);
+        blended_transform.scale = Vector3Multiply(blended_transform.scale, parent_transform.scale);
+    }
+    */
+
+    // Apply the blending matrix to the bone's transform.
+    Matrix blended_matrix = MatrixMultiply(MatrixMultiply(
+        MatrixScale(blended_transform.scale.x, blended_transform.scale.y, blended_transform.scale.z),
+        QuaternionToMatrix(blended_transform.rotation)),
+        MatrixTranslate(blended_transform.translation.x, blended_transform.translation.y, blended_transform.translation.z));
+    model->boneMatrices[bone_index] = MatrixMultiply(model->boneMatrices[bone_index], blended_matrix);
+}
+
+// Recursively process bones from the root up!
+void CalculateBoneTransform(int bone_index, Model* model, struct Actor* actor, int* solved_bones[])
+{
+    // If we are already solved just return
+    if (solved_bones[bone_index] == TRUE)
+        return;
+    // Time for recursion!
+    BoneInfo* bone = &model->skeleton.bones[bone_index];
+    if (bone->parent > -1 && !solved_bones[bone->parent]) // Solve from our parent's transform if it hasn't been solved.
+        CalculateBoneTransform(bone->parent, model, actor, solved_bones);
+    ApplyAnimationLayerTransformsToBone(bone_index, model, actor);
+    solved_bones[bone_index] = TRUE;
+}
 
 void ApplyAnimLayers(struct Actor* actor, Model* model)
 {
     if (actor->animlayer_count == -1)
         return;
-    int boneCount = model->skeleton.boneCount;
-    if (boneCount == 0)
+    if (model->skeleton.boneCount == 0)
         return;
 
-    // For each bone in the skeleton, apply the blended animation
-    for (int boneIndex = 0; boneIndex < boneCount; boneIndex++)
+    // Bones need to be calculated from the bottom up.
+    // So we do so recursively... Keeping track of solved bones so we can end recursive chains early.
+    int solved_bones[MAX_BONES] = { FALSE };
+    for (int bone_index = 0; bone_index < model->skeleton.boneCount; bone_index++)
     {
-        // Get bind pose for the bone.
-        Transform* bind_transform = &model->skeleton.bindPose[boneIndex];
-        Matrix bind_matrix = MatrixMultiply(MatrixMultiply(
-            MatrixScale(bind_transform->scale.x, bind_transform->scale.y, bind_transform->scale.z),
-            QuaternionToMatrix(bind_transform->rotation)),
-            MatrixTranslate(bind_transform->translation.x, bind_transform->translation.y, bind_transform->translation.z));
-        model->boneMatrices[boneIndex] = MatrixInvert(bind_matrix); // Apply bind pose to bone before we do any transforms
-
-        // Summed transformation of all blended animations
-        Transform blended_transform = {
-            .translation = bind_transform->translation,
-            .rotation = bind_transform->rotation,
-            .scale = bind_transform->scale
-        };
-
-        // Blend all active layers by their actual blending percents
-        for (int i = 0; i <= actor->animlayer_count; i++)
-        {
-            if (CHECK_SKIP_LAYER(actor->animation_layers[i]))
-                continue;
-            // Get layer's data
-            struct AnimationLayer* layer = actor->animation_layers[i];
-            if (!layer->bone_filter[boneIndex]) // Layer is disabled by this bone
-                continue;
-            ModelAnimation* anim = layer->current_animation;
-
-            // Get current bone's transform at this frame of the animation
-            int frame_index = (int)layer->current_frame;
-            Transform* anim_transform = &anim->keyframePoses[frame_index][boneIndex];
-
-            // Lerp from the bind pose to the desired transform. This will act as our influence of intensity.
-            blended_transform.translation = Vector3Lerp(blended_transform.translation, anim_transform->translation, layer->blend_factor);
-            blended_transform.rotation = QuaternionSlerp(blended_transform.rotation, anim_transform->rotation, layer->blend_factor);
-            blended_transform.scale = Vector3Lerp(blended_transform.scale, anim_transform->scale, layer->blend_factor);
-        }
-
-        // Build and apply the influence scaled matrix to the bone's transform.
-        Matrix blended_matrix = MatrixMultiply(MatrixMultiply(
-            MatrixScale(blended_transform.scale.x, blended_transform.scale.y, blended_transform.scale.z),
-            QuaternionToMatrix(blended_transform.rotation)),
-            MatrixTranslate(blended_transform.translation.x, blended_transform.translation.y, blended_transform.translation.z));
-        model->boneMatrices[boneIndex] = MatrixMultiply(model->boneMatrices[boneIndex], blended_matrix);
+        CalculateBoneTransform(bone_index, model, actor, &solved_bones);
     }
 
     // Forward bones to gpu
