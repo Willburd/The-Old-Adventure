d5977200c125c34a2450f2d2a174ca65ca15fc13
Will <7099514+Willburd@users.noreply.github.com>
2026-06-21 04:58:49 -0400

    more defines

--- src/hud.h
+++ src/hud.h
@@ -10,9 +10,14 @@
 
 #define HUD_WIDTH renderWidth
 #define HUD_HEIGHT renderHeight
+
 #define HUD_WIDTHHALF (renderWidth * 0.5f)
 #define HUD_HEIGHTHALF (renderHeight * 0.5f)
 #define HUD_WIDTHQUARTER (renderWidth * 0.25f)
 #define HUD_HEIGHQUARTER (renderHeight * 0.25f)
+#define HUD_WIDTHEIGTH (renderWidth * 0.125f)
+#define HUD_HEIGHEIGTH (renderHeight * 0.125f)
+#define HUD_WIDTHTENTH (renderWidth * 0.1f)
+#define HUD_HEIGHTTENTH (renderHeight * 0.1f)
 
 #endif
