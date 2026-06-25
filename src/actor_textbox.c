61f77bccb5c18d7841c4d45071459b394f0edb94
Will <7099514+Willburd@users.noreply.github.com>
2026-06-25 03:42:27 -0400

    textbox interaction

--- src/actor_textbox.c
+++ src/actor_textbox.c
@@ -10,17 +10,19 @@
 
 typedef struct
 {
-	unsigned int counter;
 	unsigned int text_start_index;
 	unsigned int text_end_index;
 	char* current_text;
 	char current_token;
+	float text_progress;
+	float text_speed;
 } TextboxData;
 
 // private header
 ACTOR_UPDATE(textbox);
 ACTOR_POSTDRAWHUD(textbox);
 ACTOR_CLEANUP(textbox);
+int GetSegmentLength(struct Actor* textbox);
 void ProcessToken(struct Actor* textbox);
 void AdvanceText(struct Actor* textbox);
 
@@ -46,19 +48,21 @@ ACTOR_INIT(textbox)
 	// Set data
 	MALLOC_ACTOR_DATA(TextboxData, actor->data);
 	TextboxData* textbox_data = (TextboxData*)actor->data;
-	textbox_data->counter = 0;
 	textbox_data->text_start_index = 0;
 	textbox_data->text_end_index = 0;
 	textbox_data->current_text = "?";
-	textbox_data->current_token = "A";
+	textbox_data->current_token = 'A';
+	textbox_data->text_progress = 0.0f;
+	textbox_data->text_speed = TEXTBOX_DEFAULT_SPEED;
 }
 
-struct Actor* TEXTBOX_CREATE(struct Actor* owner, struct Actor* player, char* text_id)
+struct Actor* TEXTBOX_CREATE(struct Actor* owner, struct Actor* player, char* text_id, float text_speed)
 {
 	struct Actor* textbox = ACTOR_FACTORY(act_textbox, NULL, Vector3Zero(), QuaternionIdentity(), Vector3One(), Vector3Zero());
 	TextboxData* textbox_data = (TextboxData*)textbox->data;
 	textbox_data->current_text = GetText(text_id);
-	AdvanceText(textbox);
+	textbox_data->text_speed = text_speed;
+	ProcessToken(textbox);
 	return textbox;
 }
 
@@ -69,20 +73,23 @@ struct Actor* TEXTBOX_CREATE(struct Actor* owner, struct Actor* player, char* te
 ACTOR_UPDATE(textbox)
 {
 	TextboxData* textbox_data = (TextboxData*)actor->data;
-	textbox_data->counter += 1;
-
-	if (textbox_data->counter < 5)
+	textbox_data->text_progress += textbox_data->text_speed;
+	if (textbox_data->text_progress < 1)
 		return;
 
 	if (CHECK_INPUTPRESSED(input_interact))
 	{
-		AdvanceText(actor);
-	}
-
-	if (textbox_data->text_start_index >= strlen(textbox_data->current_text))
-	{
-		ACTOR_DESTROY(actor);
-		return;
+		unsigned int segment_length = GetSegmentLength(actor);
+		if (textbox_data->text_progress < segment_length)
+		{
+			// Text skip
+			textbox_data->text_progress = 10000.0f;
+		}
+		else
+		{
+			// Next
+			ProcessToken(actor);
+		}
 	}
 }
 
@@ -104,21 +111,40 @@ ACTOR_POSTDRAWHUD(textbox)
 	DrawRectangle(left, top, text_box_width, text_box_height, BLACK);
 
 	// Draw the current text segment
-	unsigned int segment_length = (textbox_data->text_end_index - textbox_data->text_start_index) - 3; // 3 chars are from token + eol
+	unsigned int segment_length = GetSegmentLength(actor);
+	unsigned int draw_length = min(segment_length, textbox_data->text_progress);
+
 	char substr[2048] = { 0 };
-	memcpy(substr, textbox_data->current_text + (sizeof(char) * textbox_data->text_start_index), sizeof(char) * segment_length);
+	memcpy(substr, textbox_data->current_text + (sizeof(char) * textbox_data->text_start_index), sizeof(char) * draw_length);
 	DrawTextEx(default_font, substr, (Vector2) { left + 10, top + 5 }, TEXTBOX_DEFAULT_SIZE, 1, WHITE);
+}
+
+int GetSegmentLength(struct Actor* textbox)
+{
+	TextboxData* textbox_data = (TextboxData*)textbox->data;
+	return (textbox_data->text_end_index - textbox_data->text_start_index) - 3; // 3 chars are from token + eol
 }
 
 void ProcessToken(struct Actor* textbox)
 {
 	TextboxData* textbox_data = (TextboxData*)textbox->data;
+	switch (textbox_data->current_token)
+	{
+		default:
+		case 'A': // Advance
+			AdvanceText(textbox);
+			break;
+
+		case 'E': // End
+			ACTOR_DESTROY(textbox);
+			break;
+	}
 }
 
 void AdvanceText(struct Actor* textbox)
 {
 	TextboxData* textbox_data = (TextboxData*)textbox->data;
-	textbox_data->counter = 0; // Reset timer
+	textbox_data->text_progress = 0.0f;
 	textbox_data->text_start_index = textbox_data->text_end_index;
 
 	// advance the starting point to where we last ended off
@@ -132,11 +158,10 @@ void AdvanceText(struct Actor* textbox)
 				return;
 
 			case '|': // Process a token for custom text effects
-				textbox_data->text_end_index++; // Skip token flag
+				textbox_data->text_end_index++; // Skip token indicator
 				textbox_data->current_token = textbox_data->current_text[textbox_data->text_end_index];
-				ProcessToken(textbox);
-				textbox_data->text_end_index++; // skip the token
-				textbox_data->text_end_index++; // skip the eol
+				textbox_data->text_end_index++; // skip the token itself
+				textbox_data->text_end_index++; // skip the eol char
 				return;
 
 			default: // Next character
--- src/actor_textbox.h
+++ src/actor_textbox.h
@@ -6,6 +6,8 @@
 
 #define TEXTBOX_DEFAULT_SIZE 16
 
-struct Actor* TEXTBOX_CREATE(struct Actor* owner, struct Actor* player, char* text_id);
+#define TEXTBOX_DEFAULT_SPEED 0.6f
+
+struct Actor* TEXTBOX_CREATE(struct Actor* owner, struct Actor* player, char* text_id, float text_speed);
 
 #endif
