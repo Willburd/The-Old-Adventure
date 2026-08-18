7bd71e8b48bcca68daa81f7dc1e0eb509c070c58
Will <7099514+Willburd@users.noreply.github.com>
2026-08-18 09:17:51 -0400

    note todo

--- src/advluts.h
+++ src/advluts.h
@@ -10,7 +10,7 @@ void AdvLUTShaderUniforms(struct PostProcessingLayer* data, RenderTexture2D* ren
     Texture2D* tex = AssetGet_Texture(ASSET_TEXTURES"/LUTs/Neutral.png");
     Texture2D* screen_tex = &render_tex->texture;
 
-    data->material->maps[MATERIAL_MAP_ALBEDO].texture = *screen_tex;
+    data->material->maps[MATERIAL_MAP_ALBEDO].texture = *screen_tex; // TODO - Find out how to properly respect material maps and samplers
 
     int loc = GetShaderLocation(shdr, "lut_tex");
     SetShaderValueTexture(shdr, loc, *tex);
