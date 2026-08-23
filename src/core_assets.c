2036798bb38f7122f6ecf58cbb807ad8d802ee9c
Will <7099514+Willburd@users.noreply.github.com>
2026-08-23 11:56:04 -0400

    new core materials

--- src/core_assets.c
+++ src/core_assets.c
@@ -11,12 +11,16 @@ void LoadCoreAssets()
     // Load default textures
     LoadAsset_Texture(ASSET_TEXTURES"/Engine/no_texture.png", TRUE, NULL);
     LoadAsset_Texture(ASSET_TEXTURES"/Engine/no_material.png", TRUE, NULL);
+    LoadAsset_Texture(ASSET_TEXTURES"/Engine/white.png", TRUE, NULL);
+    LoadAsset_Texture(ASSET_TEXTURES"/Engine/black.png", TRUE, NULL);
     LoadAsset_Texture(ASSET_TEXTURES"/Engine/example.png", TRUE, NULL);
     // TODO - Your default textures here
 
     // Load default materials
     LoadAsset_Material(ASSET_MATERIALS"/Engine/no_material.mat", TRUE);
     LoadAsset_Material(ASSET_MATERIALS"/Engine/example.mat", TRUE);
+    LoadAsset_Material(ASSET_MATERIALS"/Engine/black.mat", TRUE);
+    LoadAsset_Material(ASSET_MATERIALS"/Engine/white.mat", TRUE);
     LoadAsset_Material(ASSET_MATERIALS"/Engine/collision_debug.mat", TRUE);
     // TODO - Your default materials here
 
